5e286ca3f9ec5285b767cd9bebe6daaa40a410e2
mintuchel <max3627@naver.com>
2023-12-07 03:00:09 +0900

    [FIX] CHARIZARD FIREBALL
    - fixed makeFireground bug near the border lines of the AREA
    - fixed PCNear bug
    - increased FIREGROUND_SEC for more long-lasting firegrounds

--- DrillOfTheUnderWorld/CharizardFireball.hpp
+++ DrillOfTheUnderWorld/CharizardFireball.hpp
@@ -1,7 +1,7 @@
 #ifndef __CHARIZARD_FIREBALL_
 #define __CHARIZARD_FIREBALL_
 
-#define FIREGROUND_SEC 6
+#define FIREGROUND_SEC 30
 
 #include "NPC.hpp"
 #include <vector>
@@ -19,7 +19,7 @@ class CharizardFireball : public NPC {
 public:
 	double dx, dy;
 	int movingtime;
-	int fireground[3][3];
+	bool fireground[3][3];
 
 	// vector for fireground image indexs
 	vector<int> fireground_indexes;
@@ -37,8 +37,6 @@ public:
 	void initFireground();
 	void makeFireground();
 	void eraseFireground();
-
-	int getDistance(int x1, int x2, int y1, int y2);
 };
 
 CharizardFireball::CharizardFireball(int x, int y) : NPC(x, y, 0, 20, 1) {
@@ -60,26 +58,30 @@ CharizardFireball::CharizardFireball(int x, int y) : NPC(x, y, 0, 20, 1) {
 	imageArray[imageLayer.imageCount++] = { bmpNameFireball, x, y, 1 };
 }
 
-int CharizardFireball::getDistance(int x1, int y1, int x2, int y2) {
-	return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
-}
-
 // check if PC is on Fireground of each Fireball
 bool CharizardFireball::PCNear() {
 	int PC_X = imageLayer.images[0].x;
 	int PC_Y = imageLayer.images[0].y;
 
-	// if two circles contact each other 
-	for (int i = 0; i < fireground_indexes.size(); i++) {
-		Image curground = imageArray[fireground_indexes[i]];
-		// circle's center point
-		int CENTER_X = curground.x - BLOCKSIZE / 2;
-		int CENTER_Y = curground.y - BLOCKSIZE / 2;
+	int startX = x - BLOCKSIZE;
+	int startY = y - BLOCKSIZE;
 
-		if (getDistance(PC_X, CENTER_X, PC_Y, CENTER_Y) < BLOCKSIZE) {
-			return true;
+	int curY, curX;
+
+	for (int i = 0; i < 3; i++) {
+		for (int k = 0; k < 3; k++) {
+			// if false dont check
+			if (!fireground[i][k]) continue;
+
+			curY = startY + BLOCKSIZE * i;
+			curX = startX + BLOCKSIZE * k;
+
+			if (PC_X >= curX && PC_X <= curX + BLOCKSIZE && PC_Y >= curY && PC_Y <= curY + BLOCKSIZE) {
+				return true;
+			}
 		}
 	}
+
 	return false;
 }
 
@@ -87,7 +89,8 @@ bool CharizardFireball::PCNear() {
 void CharizardFireball::move() {
 
 	// check the 9 blocks of fireground area
-	if (PCNear()) {
+
+	if (PCNear() && cnt >= movingtime && cnt <= movingtime + FIREGROUND_SEC) {
 		attack();
 	}
 
@@ -108,7 +111,7 @@ void CharizardFireball::move() {
 	if (cnt == movingtime) {
 		makeFireground();
 	}
-	else if (cnt > movingtime + FIREGROUND_SEC) {
+	else if (cnt == movingtime + FIREGROUND_SEC) {
 		eraseFireground();
 	}
 
@@ -125,8 +128,8 @@ void CharizardFireball::initFireground() {
 			fireground[i][k] = rand() % 2;
 		}
 	}
-	// center is always 1
-	fireground[1][1] = 1;
+	// center is always true
+	fireground[1][1] = true;
 }
 
 void CharizardFireball::makeFireground() {
@@ -134,33 +137,27 @@ void CharizardFireball::makeFireground() {
 	int startX = x - BLOCKSIZE;
 	int startY = y - BLOCKSIZE;
 
-	//int infoX = convertPosToInfoX(startX);
-	//int infoY = convertPosToInfoY(startY);
-
 	int curX, curY;
 
 	// i is y and k is x
 	for (int i = 0; i < 3; i++) {
 		for (int k = 0; k < 3; k++) {
 
-			if (fireground[i][k] == 0) continue;
+			// if false dont check
+			if (!fireground[i][k]) continue;
+
 			curY = startY + BLOCKSIZE * i;
 			curX = startX + BLOCKSIZE * k;
 
 			// check range for outofbounds
-			if (curX < BLOCKSIZE || curX > 1200 - BLOCKSIZE || curY < BLOCKSIZE || curY > 1200 - BLOCKSIZE) continue;
+			//if (curX < BLOCKSIZE || curX > 1200 - BLOCKSIZE || curY < BLOCKSIZE || curY > 1200 - BLOCKSIZE) continue;
 
-			for (int dy = 0; dy < BLOCKSIZE; dy++) {
-				for (int dx = 0; dx < BLOCKSIZE; dx++) {
-					if (blockInfo[convertPosToInfoY(curY + dy)][convertPosToInfoX(curX + dx)] != 0) {
-						fireground[i][k] = 0;
-						goto skip;
-					}
-				}
-			}
 
-		skip:
-			if (fireground[i][k]) {
+			if (curX < AREA_ORIGIN_X + BLOCKSIZE || curX > AREA_ORIGIN_X + 1200 - BLOCKSIZE
+				|| curY < AREA_ORIGIN_Y + BLOCKSIZE || curY > AREA_ORIGIN_Y + 1200 - BLOCKSIZE) continue;
+
+
+			if (!collisionCheck(curX + dx, curY + dy)) {
 				fireground_indexes.push_back(imageLayer.imageCount);
 				imageArray[imageLayer.imageCount++] = { bmpNameFireground, curX, curY, 1 };
 			}
